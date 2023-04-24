c626bcf5e1727bbfca80dc227e6a1c2d14b944b1
Jack Clarke <jackfulcher09@gmail.com>
2023-04-24 16:59:12 -0400

    reveal hidden files

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -10,7 +10,14 @@ return {
     "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
     "MunifTanjim/nui.nvim",
   },
-  config = function()
-    require('neo-tree').setup {}
-  end,
+  opts = {
+    filesystem = {
+      filtered_items = {
+        visible = true,
+        --show_hidden_count = true,
+        hide_dotfiles = false,
+        hide_gitignored = false,
+      },
+    }
+  }
 }
